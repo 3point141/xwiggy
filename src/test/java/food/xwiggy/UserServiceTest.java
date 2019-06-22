d99328a1b90398b56c9ed6c7daf30bfeb599931c
3point141 <aman95kumar@gmail.com>
2019-06-23 00:07:01 +0530

    added unit test for db check

--- src/test/java/food/xwiggy/UserServiceTest.java
+++ src/test/java/food/xwiggy/UserServiceTest.java
@@ -1,11 +1,23 @@
 package food.xwiggy;
 
-//@ContextConfiguration(locations = { "classpath:xwiggy/config/user-beans.xml" })
-//@RunWith(SpringJUnit4ClassRunner.class)
+import food.xwiggy.model.Login;
+import food.xwiggy.model.User;
+import food.xwiggy.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@ContextConfiguration(locations = { "classpath:xwiggy/config/user-beans.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
 public class UserServiceTest {
 
- /*  @Autowired
-  UserService userService;
+    @Autowired
+    UserService userService;
 
   @Before
   public void setUp() throws Exception {
@@ -15,12 +27,16 @@ public class UserServiceTest {
   public void testValidateUser() {
 
     Login login = new Login();
-    login.setUsername("ranjith");
-    login.setPassword("sekar");
+    login.setUsername("amank");
+    login.setPassword("abcd1234");
 
     User user = userService.validateUser(login);
 
-    assertEquals("Ranjith", user.getFirstname());
-  }*/
+    assertEquals("Aman", user.getFirstname());
+    assertEquals("Kumar",user.getLastname());
+    assertEquals("aman95kumar@gmail.com",user.getEmail());
+    assertEquals("221b",user.getAddress());
+    assertEquals(1234567890,user.getPhone());
+  }
 
 }
